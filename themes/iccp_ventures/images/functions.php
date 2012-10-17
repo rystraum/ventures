ce40d8d8ad80d7933dd4251cba0db218efbab4a8
Rystraum Gamonez <rystraum@gmail.com>
2012-10-18 01:57:45 +0800

    initial setup

---
+++ themes/iccp_ventures/images/functions.php
@@ -0,0 +1,7 @@
+<?php
+// banner randomizer
+$arr = array("1.jpg","2.jpg","3.jpg","4.jpg");
+$random = rand(0,4);
+$filen = $arr[$random];
+
+?>
---
+++ themes/iccp_ventures/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>ICCP Venture Partners</title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+</head>
+
+<body>
+<!--[if IE]>
+<div id="IEroot">
+<![endif]-->
+<div class="main">
+  <div class="float header">
+    <div class="float lefthd">&nbsp;</div>
+    <div class="float logohd"><img src="<?php echo get_bloginfo('template_directory') ?>/images/IVPI_logo.jpg" alt="" width="275" height="82" /></div>
+    <div class="float righthd"><img src="<?php echo get_bloginfo('template_directory') ?>/images/Tagline.jpg" alt="" width="256" height="19" /></div>
+  </div>
+  <div class="float container">
+    <div class="float centerlefthd">
+        <div class="centerlefthdcontent">&nbsp;</div>
+        <div class="centerlefthdcontent2">&nbsp;</div>
+        <div class="centerlefthdcontent3">&nbsp;</div>
+        <div class="centerlefthdcontent4">&nbsp;</div>
+    </div>
+    <div class="float centermidhd">
+      <div class="centermidhdcontent">&nbsp;</div>
+      <div class="centermidhdcontentxv2">
+        <div align="left"><img src="<?php echo get_bloginfo('template_directory') ?>/images/home_positionStatement[1].JPG" alt="" width="274" height="198" />
+              </div>
+    </div>
+      <div class="centermidhdcontent3">&nbsp;</div>
+      <div class="float centermidhdcontent4 bucket1"><a href="experiencedinvestmentteam.html"><img src="<?php echo get_bloginfo('template_directory') ?>/images/eet.jpg" alt="" width="275" height="82" border="0" /></a></div>
+      <div class="float centermidhdcontent4 bucket2"><a href="exceptionalperformance.html"><img src="<?php echo get_bloginfo('template_directory') ?>/images/exceptional_performance.jpg" alt="" width="275" height="81" border="0" /></a></div>
+          <div class="float centermidhdcontent4 bucket2"><a href="establishednetwork.html"><img src="<?php echo get_bloginfo('template_directory') ?>/images/LeftNav_EstablishedNetwork.jpg" alt="" width="275" height="81" border="0" /></a></div>
+         <div class="float centermidhdcontent4 bucket5"> <div class="bucket4image"><a href="news.html"><img src="<?php echo get_bloginfo('template_directory') ?>/images/news_new.jpg" alt="News" width="276" height="34" border="0" /></a></div>
+       <div class="bucket4news"><?php echo $newsrow['newsTitle']; ?></div>
+       <div class="bucket4news2"><?php echo $newsrow['newsCaption']; ?></div>
+        <div class="bucket4news3"><a href="news.html">>> More</a></div>
+      <div class="bucket6">&nbsp;</div>
+      
+       </div>
+       
+    </div>
+    <div class="float centerrighthd">
+      <ul id="nav">
+      <li><a href="index.html" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Home','','<?php echo get_bloginfo('template_directory') ?>/images/home_grey.gif',1)"><img name="Home" src="<?php echo get_bloginfo('template_directory') ?>/images/home_grey.gif" alt="Home" width="106" height="37" border="0" /></a></li>
+      <li><a href="#ph" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('About Us','','<?php echo get_bloginfo('template_directory') ?>/images/aboutus_grey.gif',1)" ><img name="About Us" src="<?php echo get_bloginfo('template_directory') ?>/images/aboutus_blue.gif" alt="About Us" width="108" height="37" border="0" /></a>
+        <ul >
+          <li><a href="aboutus_relationship.html" class="class1 top"><img src="<?php echo get_bloginfo('template_directory') ?>/images/AboutUs_Overview.jpg" border="0" width="109" height="30"/></a></li>
+          <li><a href="milestones.html" class="class2"><img src="<?php echo get_bloginfo('template_directory') ?>/images/AboutUs_Milestones.jpg" border="0" width="109" height="32"/></a></li>
+          <li><a href="ph-us_advantages.html" class="class2"><img src="<?php echo get_bloginfo('template_directory') ?>/images/AboutUs_ThePhilippines.jpg" border="0" width="109" height="32"/></a></li>
+          <li><a href="news.html"><img src="<?php echo get_bloginfo('template_directory') ?>/images/AboutUs_News.jpg" border="0" /></a></li>
+        </ul> 
+      
+      </li>
+      <li><a href="#ph" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('How We Work','','<?php echo get_bloginfo('template_directory') ?>/images/howwework_grey.gif',1)" ><img name="How We Work" src="<?php echo get_bloginfo('template_directory') ?>/images/howwework_blue.gif" alt="How We Work" width="108" height="37" border="0" /></a>
+        <ul >
+          <li><a href="invobjectives.html" class="class1 top"><img src="<?php echo get_bloginfo('template_directory') ?>/images/HowWeWork_Objectives.jpg" border="0" /></a></li>
+          <li><a href="invphilosophy.html" class="class1"><img src="<?php echo get_bloginfo('template_directory') ?>/images/HowWeWork_Philosophy.jpg" border="0" /></a></li>
+          <li><a href="invstrategy.html" class="class1"><img src="<?php echo get_bloginfo('template_directory') ?>/images/HowWeWork_Strategy.jpg" border="0" /></a></li>
+          <li><a href="invcriteria.html" class="class1"><img src="<?php echo get_bloginfo('template_directory') ?>/images/HowWeWork_Criteria.jpg" border="0" /></a></li>
+          <li><a href="invprocess.html" class="class1"><img src="<?php echo get_bloginfo('template_directory') ?>/images/HowWeWork_Process.jpg" border="0" /></a></li>
+        </ul> 
+      
+      </li>
+      <li><a href="team.html" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Team','','<?php echo get_bloginfo('template_directory') ?>/images/team_grey.gif',1)"><img name="Team" src="<?php echo get_bloginfo('template_directory') ?>/images/team_blue.gif" alt="Team" width="108" height="37" border="0" /></a>
+        <ul >
+          <li><a href="mgtTeam.html" class="class1 top"><img src="<?php echo get_bloginfo('template_directory') ?>/images/Team_Management.jpg" border="0" /></a></li>
+          <li><a href="investmentcmtee.html" class="class1"><img src="<?php echo get_bloginfo('template_directory') ?>/images/Team_Investment.jpg" border="0" /></a></li>
+          <li><a href="advisoryboard.html" class="class1"><img src="<?php echo get_bloginfo('template_directory') ?>/images/Team_Advisory.jpg" border="0" /></a></li>
+        </ul> 
+      </li>
+      <li ><a href="portfolio.php?vid=1" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Portfolio','','<?php echo get_bloginfo('template_directory') ?>/images/portfolio_grey.gif',1)" ><img name="Portfolio" src="<?php echo get_bloginfo('template_directory') ?>/images/portfolio_blue.gif" alt="Portfolio" width="107" height="37" border="0" /></a>
+        <ul >
+          <li><a href="portfolio.php?vid=1" class="class1 top"><img src="<?php echo get_bloginfo('template_directory') ?>/images/Portfolio_CoreTechnology.jpg" border="0" /></a></li>
+          <li><a href="portfolio.php?vid=2" class="class1"><img src="<?php echo get_bloginfo('template_directory') ?>/images/Portfolio_Electronics.jpg" border="0" /></a></li>
+          <li><a href="portfolio.php?vid=3" class="class1"><img src="<?php echo get_bloginfo('template_directory') ?>/images/Portfolio_Outsourcing.jpg" border="0" /></a></li>
+          <li><a href="portfolio.php?vid=4" class="class1"><img src="<?php echo get_bloginfo('template_directory') ?>/images/Portfolio_Software.jpg" border="0" /></a></li>
+          <li><a href="portfolio.php?vid=5" class="class1"><img src="<?php echo get_bloginfo('template_directory') ?>/images/Portfolio_Products.jpg" border="0" /></a></li>
+        </ul> 
+      </li>
+      <li><a href="contactus.html" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Contact Us','','<?php echo get_bloginfo('template_directory') ?>/images/contactus_grey.gif',1)"><img name="Contact Us" src="<?php echo get_bloginfo('template_directory') ?>/images/contactus_blue.gif" alt="Contact Us" width="110" height="37" border="0" /></a></li>
+      </ul>
+      
+    </div>
+    
+    <div class="floatright centerrightmain"><img src="<?php echo get_bloginfo('template_directory') ?>/images/Banner_Home.jpg" alt="Home" width="647" height="200" /></div>
+    <div class="floatright centerrightmain2">&nbsp;</div>
+    <div class="floatright centerrightmain3"><span class="txtblue">Welcome<br />
+    </span>
+      <hr />
+      <p class="txtgrey"><strong>ICCP  Venture Partners</strong> is an established &ldquo;Trans-Pacific&rdquo; venture capital  management company that leverages U.S. technological innovation with  Asian cost-effective contract manufacturing and business process outsourcing to  create extraordinary profits for our investors.&nbsp; </p>
+      <ul>
+        <li class="txtgrey">For the past 10 years, we have demonstrated to be a  value-added interface for our portfolio companies &ndash; we have focused on  unlocking value by facilitating cross-border linkages and/or synergies among  companies in both geographies.&nbsp;</li>
+        </ul>
+      <ul>
+        <li class="txtgrey">We have proven our ability to introduce and  strategically link our U.S.-based portfolio companies with our Asian partners  through our offices in Manila, Philippines and Palo Alto, California.&nbsp; For our U.S.-based portfolio companies, we  have provided linkage to potential customers, contract manufacturers, and  outsourced and other service providers in Asia.  For our Asia-based portfolio companies, we have provided introductions to  potential customers and technology providers in the U.S. </li>
+        </ul>
+      <ul>
+        <li class="txtgrey">We invest in early to expansion stage companies in  high-growth industries located in both the U.S.  and Asia (particularly the Philippines). </li>
+        </ul>
+      <p class="txtgrey"><a href="http://www.alfafusion.com/ivpiv3/experiencedinvestmentteam.html">&gt;&gt;  Read more about our Investment Team </a></p>
+     
+    </div>
+  </div>
+  <div class="float footerleft"><span class="txtwhitefooterleft"><a href="disclaimer.html">Disclaimer</a> | <a href="privacypolicy.html">Privacy Policy</a> | <a href="sitemap.html">Sitemap </a></span></div>
+  <div class="float footerright"><span class="txtwhitefooter">Copyright &copy; 2008 ICCP Venture Partners </span></div>
+</div>
+
+
+<div id="preloaded-images">
+  <img src="<?php echo get_bloginfo('template_directory') ?>/images/home_grey.gif" width="1" height="1"/>
+  <img src="<?php echo get_bloginfo('template_directory') ?>/images/aboutus_grey.gif" width="1" height="1"/>
+  <img src="<?php echo get_bloginfo('template_directory') ?>/images/team_grey.gif" width="1" height="1"/>
+  <img src="<?php echo get_bloginfo('template_directory') ?>/images/portfolio_grey.gif" width="1" height="1"/>
+  <img src="<?php echo get_bloginfo('template_directory') ?>/images/contactus_grey.gif" width="1" height="1"/>
+  <img src="<?php echo get_bloginfo('template_directory') ?>/images/howwework_grey.gif" width="1" height="1"/>
+</div>
+<!--[if IE]>
+</div>
+<![endif]-->
+</body>
+</html>
