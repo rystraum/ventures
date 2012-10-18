cd49de00a6d3e40e61a3f9fb2139cc3ee657c426
Rystraum Gamonez <rystraum@gmail.com>
2012-10-18 10:56:51 +0800

    styling menu

--- themes/iccp_ventures/header.php
+++ themes/iccp_ventures/header.php
@@ -49,7 +49,7 @@
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
 			<div class="skip-link assistive-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a></div>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu') ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
